dc230d187a7803d9597d5def83df961f5a86b8c0
Nathadriel cocagne <cocagne.nathadriel@gmail.com>
2026-06-24 10:08:09 +0200

    ACTIVITE 14 : Récupérer la liste des types de bois

--- app/controllers/woodController.js
+++ app/controllers/woodController.js
@@ -1,3 +1,15 @@
+import { prisma } from '../../app.js';
+
+export const woodsList = async (req, res) => {
+  try {
+    const woods = await prisma.wood.findMany();
+    res.status(200).json(woods);
+  } catch (err) {
+    res.status(500).json({ error: err.message || 'Une erreur est survenue' });
+  }
+};
+
 export const readAll = (req, res) => {
   res.send('List of woods');
 };
+
--- app/routes/wood.js
+++ app/routes/wood.js
@@ -2,6 +2,6 @@ import express from 'express';
 const router = express.Router();
 import * as woodController from '../controllers/woodController.js';
 
-router.get('/', woodController.readAll);
+router.get('/', woodController.woodsList);
 
 export default router;
