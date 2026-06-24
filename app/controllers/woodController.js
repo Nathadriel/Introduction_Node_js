394abade90169964a370379d489cfe4c781e106f
Nathadriel cocagne <cocagne.nathadriel@gmail.com>
2026-06-24 10:45:45 +0200

    ACTIVITE 15 : Récupérer la liste des bois selon leur dureté

--- app/controllers/woodController.js
+++ app/controllers/woodController.js
@@ -9,3 +9,16 @@ export const readAll = async (req, res) => {
   }
 };
 
+export const readByHardness = async (req, res) => {
+  try {
+    const woods = await prisma.wood.findMany({
+      where: {
+        hardness: req.params.hardness,
+      },
+    });
+    res.status(200).json(woods);
+  } catch (err) {
+    res.status(500).json({ error: err.message || 'An error occurred' });
+  }
+};
+
